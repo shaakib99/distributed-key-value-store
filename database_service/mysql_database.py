ae796879ca3de92c530b56ab768ef4d85cd5a242
Wahid Shakib <wsakib87@gmail.com>
2025-02-19 21:25:17 +0600

    updated mysql_database to read and write from databasae

--- database_service/mysql_database.py
+++ database_service/mysql_database.py
@@ -1,6 +1,8 @@
 from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import sessionmaker, DeclarativeBase
 from sqlalchemy.ext.declarative import declarative_base
+from pydantic import BaseModel
+from common.exceptions import NotFoundException
 
 class MySQLDatabase:
     instance = None
@@ -16,17 +18,29 @@ class MySQLDatabase:
             MySQLDatabase.instance = MySQLDatabase(host)
         return MySQLDatabase.instance
 
-    async def create_one(self, data: dict, schema):
-        return {"message": "Hello World from MySQL database"}
+    async def create_one(self, data: BaseModel, schema: DeclarativeBase):
+        self.session.add(schema(**data.model_dump()))
+        self.session.commit()
+        return data
     
-    async def get_one(self, id: str, schema):
-        return {"message": f"Hello World from MySQL database {id}"} 
+    async def get_one(self, id: str, schema: DeclarativeBase):
+        data = self.session.query(schema).filter(schema.id == id).first()
+        if data is None:
+            raise NotFoundException(f"Data with id {id} not found")
+        return data
     
-    async def get_all(self, schema):
+    async def get_all(self, query: BaseModel, schema: DeclarativeBase):
         return {"message": "Hello World from MySQL database"}  
     
-    async def update_one(self, id: str, data: dict, schema):    
-        return {"message": f"Hello World from MySQL database {id}"}
+    async def update_one(self, id: str, data: BaseModel, schema: DeclarativeBase):
+        data = self.get_one(id, schema)
+        for key, value in data.model_dump().items():
+                setattr(data, key, value)
+        self.session.commit()
+        return data             
     
-    async def delete_one(self, id: str, schema):
-        return {"message": f"Hello World from MySQL database {id}"}
+    async def delete_one(self, id: str, schema: DeclarativeBase):
+        data = self.get_one(id, schema)
+        self.session.delete(data)
+        self.session.commit()
+        return None
